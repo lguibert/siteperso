becee1300b64bf751aab7db16a2e01e78660df8d
lguibert <lucas.guibert@viacesi.fr>
2015-06-16 16:46:14 +0200

    animation after click op

--- media/js/wheel.js
+++ media/js/wheel.js
@@ -3,6 +3,18 @@ $(document).ready(function () {
     var delta = 10;
     var actual = 0;
     var all = ["accueil", "experiences", "formation", "bdd", "programmation", "langue", "passions"];
+    var done = [false,false,false,false,false,false,false];
+
+    $('.naviton').on("click",function(){
+        var name = $(this).find("a").attr("href").split("#")[1];
+        if(!done[actual]){
+            animateContent();
+            console.log("animated with click");
+        }else{
+            console.log("already animated (used click)");
+        }
+        actual = getIndexWithName(name);
+    });
 
     //FF
     $('body').bind('DOMMouseScroll', function (e) {
@@ -36,11 +48,16 @@ $(document).ready(function () {
                 $('html, body').animate({
                     scrollTop: $($("a[href='#" + getNext(actual) + "']").attr('href')).offset().top - $("#navbar").height()
                 });
-                $("#" + getNext(actual)).addClass("animated fadeInDown").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-                    $("#" + all[actual]).find('.containerInfo').each(function(){
-                            $(this).show(0).addClass("animated zoomIn").delay(20);
-                    });
-                });
+                var index_next = getIndexWithName(getNext(actual));
+                console.log("done for index next: "+ done[index_next]);
+                if(!done[index_next]){
+                    animateContent();
+                    done[index_next] = true;
+                }else{
+                    console.log("Animation déjà effectuée pour: "+ actual);
+                    console.log(done);
+                }
+
                 actual++;
             }
         }
@@ -53,27 +70,36 @@ $(document).ready(function () {
                 $('html, body').animate({
                     scrollTop: $($("a[href='#" + getPrev(actual) + "']").attr('href')).offset().top - $("#navbar").height()
                 });
+                /*var index_actual = getIndexWithName(getNext(actual));
+                if(!done[index_actual]){
+                    animateContent();
+                    done[index_actual] = true;
+                    console.log("ok pour le up");
+                }else{
+                    console.log("Animation déjà effectuée pour: "+ index_actual);
+                    console.log(done);
+                }*/
                 actual--;
             }
         }
     }
 
+    function animateContent(){
+        $("#" + getNext(actual)).addClass("animated fadeInDown").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            console.log("end of first animation");
+            $("#" + all[actual]).find('.containerInfo').each(function(){
+                console.log("actual index: "+actual);
+                $(this).show(0).addClass("animated zoomIn").delay(20);
+            });
+        });
+    }
+
     function asNext(index) {
-        if (all[index + 1]) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return all[index + 1];
     }
 
     function asPrev(index) {
-        if (all[index - 1]) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return all[index - 1];
     }
 
     function getNext(index) {
@@ -82,6 +108,14 @@ $(document).ready(function () {
 
     function getPrev(index) {
         return all[index - 1];
+    }
+
+    function getIndexWithName(name){
+        for (data in all){
+            if (all[data] == name){
+                return parseInt(data);
+            }
+        }
     }
 
 });  
